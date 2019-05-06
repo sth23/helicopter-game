51466e0d4cc69bacc382f378f6512ce3ab9e6ca3
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-06 18:29:22 -0400

    Updated from Brython Server: 5/6/2019 6:29:25 PM

--- helicopter.py
+++ helicopter.py
@@ -27,11 +27,11 @@ class Helicopter(Sprite):
         HelicopterGame.listenKeyEvent("keyup", "up arrow", self.fall)
         
     def lift(self, event):
-        self.vy += 0.05
+        self.vy += -0.07
         self.deltavy = 0
         
     def fall(self, event):
-        self.deltavy = -0.1
+        self.deltavy = 0.1
         
     def step(self):
         self.y += self.vy
